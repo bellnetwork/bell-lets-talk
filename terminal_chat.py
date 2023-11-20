426163dfd42042358c5e8096f71977f7f3fef261
Jefferson Jerome James <78507611+belljmks@users.noreply.github.com>
2023-11-20 18:34:39 +0000

    Update terminal_chat.py

--- terminal_chat.py
+++ terminal_chat.py
@@ -1,8 +1,8 @@
 # The ultimative bell let's talk terminal
 
 import os
-import tkinter as tk
 import socketio
+import threading
 
 os.environ['TK_SILENCE_DEPRECATION'] = '1'
 
@@ -11,110 +11,71 @@ sio = socketio.Client()
 
 @sio.event
 def message(data):
-    chat_history.config(state=tk.NORMAL)
-    chat_history.insert(tk.END, "Server: " + data + "\n")
-    chat_history.config(state=tk.DISABLED)
+    print("\n" + data)
 
 @sio.event
 def connect():
-    print("I'm connected!")
+    print("Connected to the chat server!")
 
 @sio.event
 def connect_error(data):
-    print("The connection failed!")
-    print(data)
+    print("The connection failed:", data)
 
 @sio.event
 def disconnect():
-    print("I'm disconnected!")
+    print("Disconnected from the chat server")
 
-def start_chat(event=None):  # Add event=None to handle callback from bind
+def start_chat():
     global username
-    username = username_entry.get()
+    username = input("Enter your username: ")
     if username:
         # Connect to the Socket.IO server
         try:
-            sio.connect("http://ebxyb83tr3cbw.bellsocket.com", transports=['websocket'])
+            # Include the username in the connection URL
+            connection_url = f"http://ebxyb83tr3cbw.bellsocket.com?username={username}"
+            sio.connect(connection_url, transports=['websocket'])
         except Exception as e:
             print("Connection Error:", e)
             return
 
-        # Show chat window
-        login_window.destroy()
-        open_chat_window()
+        # Start the chat interface
+        open_chat_interface()
 
-def open_chat_window():
-    global chat_window, chat_history, entry
-    chat_window = tk.Tk()
-    chat_window.title("Bell Let´s Talk - " + username)
+def open_chat_interface():
+    print(f"Welcome to the chat, {username}! Type '/exit' to leave.")
+    while True:
+        message = input("Your message: ")
+        sio.emit('user_typing', {'typing': False}) # Notify server user stopped typing
+        if message.lower() == '/exit':
+            break
+        elif message.lower().startswith('/pm '):
+            parts = message.split(' ', 2)
+            if len(parts) == 3:
+                # Send private message to the server
+                sio.emit('private_message', {'target_username': parts[1], 'message': parts[2]})
+            else:
+                print("Invalid private message format. Use: /private <username> <message>")
+        elif message.lower() == '/global':
+            # Send a global switch message to the server
+            sio.emit('switch_to_global', {'username': username})
+        elif message.lower() == '/clear':
+            # Clear the terminal
+            os.system('cls' if os.name == 'nt' else 'clear')
+        elif message:
+            # Notify server user is typing
+            sio.emit('user_typing', {'typing': True})
+            # Send the message to the server
+            sio.emit('message', username + ": " + message)
+            sio.emit('user_typing', {'typing': False})  # Notify server user stopped typing
 
-    chat_frame = tk.Frame(chat_window, bg="white")
-    scrollbar = tk.Scrollbar(chat_frame)
-    chat_history = tk.Text(chat_frame, height=50, width=50, yscrollcommand=scrollbar.set, fg="white", bg="gray")
-    scrollbar.config(command=chat_history.yview)
-    chat_history.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
-    scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
-    chat_history.config(state=tk.DISABLED)
-    chat_frame.pack(expand=True, fill=tk.BOTH)
+    sio.disconnect()
 
-    entry = tk.Entry(chat_window, width=40, fg="white", bg="darkblue")
-    entry.pack(padx=10, pady=10)
-    entry.bind("<Return>", send_message)  # Bind the Enter key to send_message
-
-    send_button = tk.Button(chat_window, text="Send", command=lambda: send_message())
-    send_button.pack(pady=10)
-
-    chat_window.mainloop()
-
-def send_message(event=None):
-    message = entry.get()
-    entry.delete(0, tk.END)
-
-    if message.lower() == '/exit':
-        chat_window.destroy()
-        sio.disconnect()
-        return
-    elif message.lower().startswith('/pm '):
-        parts = message.split(' ', 2)
-        if len(parts) == 3:
-            # Send private message to the server
-            sio.emit('private_message', {'target_username': parts[1], 'message': parts[2]})
-        else:
-            chat_history.insert(tk.END, "Invalid private message format. Use: /pm <username> <message>\n")
-    elif message.lower() == '/global':
-        # Send a global switch message to the server
-        sio.emit('switch_to_global', {'username': username})
-    elif message.lower() == '/users':
-        # Send a user list request to the server
-        sio.emit('get_users')
-        # Clear the chat history
-    elif message.lower() == '/clear':
-        chat_history.config(state=tk.NORMAL)
-        chat_history.delete(1.0, tk.END)
-        chat_history.config(state=tk.DISABLED)
-    elif message.lower() == '/help':
-        chat_history.insert(tk.END, "Commands:\n")
-        chat_history.insert(tk.END, "/exit - Exit the chat\n")
-        chat_history.insert(tk.END, "/pm <username> <message> - Send a private message\n")
-        chat_history.insert(tk.END, "/global - Switch to global chat\n")
-        chat_history.insert(tk.END, "/clear - Clear the chat\n")
-        chat_history.insert(tk.END, "/help - Show this help message\n")
-        chat_history.insert(tk.END, "/users - Show the list of users\n")
-    elif message:
-        # Send the message to the server
-        sio.emit('message', username + ": " + message)
-
-# Login Window
-login_window = tk.Tk()
-login_window.title("Login")
-
-username_label = tk.Label(login_window, text="Enter Username:")
-username_label.pack()
-username_entry = tk.Entry(login_window)
-username_entry.pack()
-username_entry.bind("<Return>", start_chat)  # Bind Enter key to start_chat
-
-start_chat_button = tk.Button(login_window, text="Start Chat", command=start_chat)
-start_chat_button.pack()
-
-login_window.mainloop()
+@sio.event
+def user_typing_update(data):
+    if data['typing']:
+        print(f"\n{data['username']} is typing...")
+    else:
+        print(f"\n{data['username']} stopped typing.")
+           
+if __name__ == "__main__":
+    start_chat()
