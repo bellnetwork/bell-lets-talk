7683bbd4e6367e4e1b376319560e9717aa612039
Jefferson Jerome James <78507611+belljmks@users.noreply.github.com>
2023-11-21 02:39:23 +0000

    Update ui_chat.py

--- ui_chat.py
+++ ui_chat.py
@@ -11,16 +11,22 @@ if platform.system() == "Windows":
     
 os.environ['TK_SILENCE_DEPRECATION'] = '1'
 version = "1.0.0"
-# Create a Socket.IO client 
+# Create a Socket.IO client
 sio = socketio.Client()
 
 @sio.event
+def message(data):
+    chat_history.config(state=tk.NORMAL)
+    chat_history.insert(tk.END, "Server: " + data + "\n")
+    chat_history.config(state=tk.DISABLED)
+
+@sio.event
 def connect():
-    print("Connected to bell let's talk!")
+    print("I'm connected!")
 
 @sio.event
 def connect_error(data):
-    print("The connection failed! Reconecting...")
+    print("The connection failed!")
     print(data)
 
 @sio.event
@@ -35,27 +41,26 @@ def play_sound():
 
 @sio.event
 def message(data):
-    play_sound()
+    play_sound()  # Play sound on receiving a new message
     chat_history.config(state=tk.NORMAL)
-    if 'text' in data:
-        chat_history.insert(tk.END, data['text'] + "\n")
-    else:
-        chat_history.insert(tk.END, str(data) + "\n")
+    chat_history.insert(tk.END, "Server: " + data + "\n")
     chat_history.config(state=tk.DISABLED)
-
-chat_history = None  # Initialize as None globally
-
+    
 def start_chat(event=None):
     global username
     username = username_entry.get()
     if username:
-        # Connect to the Socket.IO server with the username and version as query parameters
+        # Connect to the Socket.IO server with the username as a query parameter
         connection_url = f"http://ebxyb83tr3cbw.bellsocket.com?username={username}&version={version}"
         try:
             sio.connect(connection_url, transports=['websocket'])
         except Exception as e:
             print("Connection Error:", e)
             return
+
+        # Show chat window
+        login_window.destroy()
+        open_chat_window()
 
 def open_chat_window():
     global chat_window, chat_history, entry
@@ -88,9 +93,6 @@ def send_message(event=None):
         chat_window.destroy()
         sio.disconnect()
         return
-    elif message.lower() == '/users':
-        # Send a user list request to the server
-        sio.emit('get_users')
     elif message.lower().startswith('/pm '):
         parts = message.split(' ', 2)
         if len(parts) == 3:
@@ -101,31 +103,25 @@ def send_message(event=None):
     elif message.lower() == '/global':
         # Send a global switch message to the server
         sio.emit('switch_to_global', {'username': username})
-    # Clear the chat history
+    elif message.lower() == '/users':
+        # Send a user list request to the server
+        sio.emit('get_users')
+        # Clear the chat history
     elif message.lower() == '/clear':
         chat_history.config(state=tk.NORMAL)
         chat_history.delete(1.0, tk.END)
         chat_history.config(state=tk.DISABLED)
-    elif message.lower() == '/version' or message.lower() == '/v':
-        chat_history.insert(tk.END, "Bell Let's Talk " + version + "\n")
-    elif message.startswith('/'):
-        chat_history.insert(tk.END, "Invalid command. Type /help to see the list of commands.\n")
-    elif message.lower() == '/help' or message.lower() == '/h':
+    elif message.lower() == '/help':
         chat_history.insert(tk.END, "Commands:\n")
         chat_history.insert(tk.END, "/exit - Exit the chat\n")
         chat_history.insert(tk.END, "/pm <username> <message> - Send a private message\n")
         chat_history.insert(tk.END, "/global - Switch to global chat\n")
         chat_history.insert(tk.END, "/clear - Clear the chat\n")
+        chat_history.insert(tk.END, "/help - Show this help message\n")
         chat_history.insert(tk.END, "/users - Show the list of users\n")
     elif message:
         # Send the message to the server
         sio.emit('message', username + ": " + message)
-
-@sio.event
-def user_list(data):
-    chat_history.config(state=tk.NORMAL)
-    chat_history.insert(tk.END, "Online Users: " + ", ".join(data) + "\n")
-    chat_history.config(state=tk.DISABLED)
 
 # Login Window
 login_window = tk.Tk()
